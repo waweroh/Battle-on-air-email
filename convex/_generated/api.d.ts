d67e39f65232ee26be0af70ee06280920d274153
waweroh <scadmsez@gmail.com>
2025-06-04 10:21:39 +0300

    add convex files

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as crons from "../crons.js";
+import type * as emailService from "../emailService.js";
 import type * as emails from "../emails.js";
 import type * as subscribers from "../subscribers.js";
 
@@ -27,6 +28,7 @@ import type * as subscribers from "../subscribers.js";
  */
 declare const fullApi: ApiFromModules<{
   crons: typeof crons;
+  emailService: typeof emailService;
   emails: typeof emails;
   subscribers: typeof subscribers;
 }>;
